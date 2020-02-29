2d782f2f39c96ecaac4bcec89a4bd8a2c9c4cb52
Arne Coucheron <arco68@gmail.com>
2020-02-29 04:29:28 +0100

    config: Don't add mini-debug-info to the boot classpath
    Change-Id: Ibce3cf86ce5534ee6a37a55188d813a219140622

--- config/go.mk
+++ config/go.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,12 +7,15 @@
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Enable whole-program R8 Java optimizations for system_server,
+# Enable whole-program R8 Java optimizations for system_server.
 FULL_SYSTEM_OPTIMIZE_JAVA := true
 
 # Reduce system server verbosity
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 PRODUCT_OTHER_JAVA_DEBUG_INFO := false
+
+# Dexpreopt
+WITH_DEXPREOPT_DEBUG_INFO := false
 
 # Use the low memory allocator to save RSS.
 MALLOC_LOW_MEMORY := true
