98736afc41303bf03f25a3d4ed0dc97028818aa1
Islam Elbanna <islamelbanna@google.com>
2024-09-26 09:26:31 +0000

    config: Use profile at framework/base/boot/ instead of the combined one at
    framework/base/config
    
    This is to decouple the framework and art profiles everywhere.
    
    `frameworks/base/boot/boot-image-profile.txt` gets added on
    `build/make/target/product/runtime_libart.mk;l=83`, and
    `art/build/boot/boot-image-profile.txt` on
    `build/make/target/product/default_art_config.mk;l=152`,
    so this just duplicates them.
    
    Bug: 260067246
    Change-Id: I233931c41c23d1a60cc4bf79ec9467b7781fe06a
    Test: running `m`

--- config/go.mk
+++ config/go.mk
@@ -7,11 +7,6 @@
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Use a profile based boot image for this device. Note that this is currently a
-# generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
