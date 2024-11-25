cad3f7073ea48efc8adb1167db7bda09ac782ffd
Giovanni Ricca <giovanniricca@proton.me>
2024-11-25 11:41:13 +0100

    config: Override `BuildVersionTags` for lineage-22 as well
    Note: LineageDesc override works on userdebug/eng builds
    
    Change-Id: I18e2331b079e8a791a44ea7075d9a9cfe62116ee

--- config/security.mk
+++ config/security.mk
@@ -16,5 +16,9 @@ ifeq ($(shell expr $(PRODUCT_VERSION_MAJOR) \< 22),1)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_VERSION_TAGS="release-keys" \
     BUILD_DISPLAY_ID="$(BUILD_ID)"
+else
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildVersionTags="release-keys" \
+    LineageDesc="$(BUILD_ID)"
 endif
 endif
