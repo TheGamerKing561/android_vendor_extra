e3d7fd2742a5e4b1123df8035b0e57b9e5f3f385
Giovanni Ricca <giovanniricca@proton.me>
2024-12-03 12:33:28 +0100

    extra: Include pixel clocks
    Change-Id: Iec13c1fea77457d1e969ee10021680e6cf30308a

--- product.mk
+++ product.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(VENDOR_EXTRA_PATH)/config/security.mk)
 # Inherit MiuiCamera Makefile
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera-$(VENDOR_EXTRA_TARGET_DEVICE)/device.mk)
 
+# Inherit Pixel clocks Makefile
+$(call inherit-product-if-exists, vendor/pixel_clocks/product.mk)
+
 # Updater
 $(call soong_config_set,lineage_extra,product_version_major,$(PRODUCT_VERSION_MAJOR))
 
