00f85498ecd9b2ba9f7b4114a944b54e9e9541ac
Giovanni Ricca <giovanniricca@proton.me>
2024-12-27 15:35:02 +0100

    config: Drop unused `PRODUCT_VERSION_MAJOR` guard
    Change-Id: Ie5aecd9413cc1e02ab70defa3666694cc1bd0cc8

--- config/security.mk
+++ config/security.mk
@@ -12,13 +12,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 # Spoof `dev-keys` builds into `release-keys`
 ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
-ifeq ($(shell expr $(PRODUCT_VERSION_MAJOR) \< 22),1)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_VERSION_TAGS="release-keys" \
-    BUILD_DISPLAY_ID="$(BUILD_ID)"
-else
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildVersionTags="release-keys" \
     LineageDesc="$(BUILD_ID)"
-endif
 endif
