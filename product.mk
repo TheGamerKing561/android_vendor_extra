9a783c38a0048111241c8729fa333ea7f29e24a8
Giovanni Ricca <giovanniricca@proton.me>
2025-01-02 15:49:51 +0100

    extra: Enable Secure NFC for selected supported SKUs
    > joyeuse
    > lisa
    > moonstone_p_global
    > sunstone_global
    
    Change-Id: Ibbc3a3fc6fb929cd60a12412df5369195937ac0d

--- product.mk
+++ product.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     neofetch
 
+PRODUCT_PACKAGES += \
+    init.nfc_extra.rc
+
 # Iperf3
 PRODUCT_PACKAGES += \
     iperf3
@@ -45,6 +48,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkOverlayEXTRA \
+    NfcOverlayEXTRA \
     SettingsOverlayEXTRA \
     SimpleDeviceConfigOverlayEXTRA \
     SystemUIOverlayEXTRA \
