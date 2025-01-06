4fa27915d1005068ef2cfd9caed1c6641a870444
Giovanni Ricca <giovanniricca@proton.me>
2025-01-06 11:25:58 +0100

    vendorsetup: Increase core counts
    Before:
      Asus
      - 20 / 2 + 2 = 12
      cringe-gen
      - 60 / 2 + 2 = 32
    
    After: Asus
      - 20 / 2 + 4 = 14
      cringe-gen
      - 60 / 2 + 4 = 34
    
    Change-Id: I7bf2c2864b2fe43bfcd6d1702bfc1b5eac0fd8fd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Shebang is intentionally missing - do not run as a script
 
-# Copyright (C) 2024 Giovanni Ricca
+# Copyright (C) 2024-2025 Giovanni Ricca
 # SPDX-License-Identifier: Apache-2.0
 
 # Enable ccache
@@ -241,7 +241,7 @@ mka_build() {
 
     [[ "${DIRTY_BUILD}" != "true" ]] && mka installclean
 
-    while ! mka bacon -j$(($(nproc) / 2 + 2)); do
+    while ! mka bacon -j$(($(nproc) / 2 + 4)); do
         LOGE "bacon failed!"
         return 0
     done
@@ -291,7 +291,7 @@ mka_kernel() {
 
     kernel_targets=${device_kernel_targets[$DEVICE]:-"bootimage"}
 
-    while ! mka ${kernel_targets} -j$(($(nproc) / 2 + 2)); do
+    while ! mka ${kernel_targets} -j$(($(nproc) / 2 + 4)); do
         LOGE "${kernel_targets} failed!"
         return 0
     done
