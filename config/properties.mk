71fe5729285ddcffd38080e4157ed771be6ef53b
Giovanni Ricca <giovanniricca@proton.me>
2025-02-14 10:43:35 +0100

    Revert "extra: props: Enable usage of dex2oat64"
    This reverts commit bf8e04ef5311a5ed270fc4d52385fe04be097fe5.
    
    Change-Id: Ib8c9e3d98d4634ecfea8281bcd5650d201d34a7e

--- config/properties.mk
+++ config/properties.mk
@@ -7,10 +7,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash #\
     #service.adb.tcp.port=5555
 
-# Use 64-bit dex2oat for better dexopt time.
-PRODUCT_PRODUCT_PROPERTIES += \
-    dalvik.vm.dex2oat64.enabled=true
-
 # Too many tombstones can cause bugreports to grow too large to be uploaded.
 PRODUCT_PRODUCT_PROPERTIES += \
     tombstoned.max_tombstone_count=10
