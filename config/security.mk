684838d8fce6630876998f198a162453522972d6
Giovanni Ricca <giovanniricca@proton.me>
2025-02-19 19:05:53 +0100

    config: Stop spoofing "release-keys"
    Topic: inline-release-keys
    
    Change-Id: I73d3b21b0e1c287a8ad9f2d05e74f249c2607d34

--- config/security.mk
+++ config/security.mk
@@ -9,9 +9,7 @@ VENDOR_EXTRA_PUBLIC_KEYS_PATH := vendor/extra/build/target/product/security
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(VENDOR_EXTRA_PUBLIC_KEYS_PATH)/mindthegapps
 
-# Spoof `dev-keys` builds into `release-keys`
 ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildVersionTags="release-keys" \
     LineageDesc="$(BUILD_ID)"
 endif
