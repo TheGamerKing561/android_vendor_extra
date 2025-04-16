3e33868a2602ad955bfede19fdc8524f9e82570f
Giovanni Ricca <giovanniricca@proton.me>
2025-04-16 15:30:48 +0200

    tools: releases: Hack for daisy
    Change-Id: Ib88a3a07eb6b7b08e0641048e62e94141b0db6a0

--- tools/releases/los_ota_json.py
+++ tools/releases/los_ota_json.py
@@ -85,8 +85,36 @@ ota = f"""{{
 }}
 """
 
+# TODO: Nuke it on LineageOS 23
+ota_daisy = f"""{{
+  "response": [
+    {{
+      "datetime": {datetime},
+      "filename": "{filename}",
+      "id": "{id}",
+      "romtype": "unofficial",
+      "size": {size},
+      "url": "{url}",
+      "version": "23.0"
+    }}
+  ]
+}}
+"""
+
 for ota_json_file in ota_path.glob('*.json'):
-    ota_json_file.write_text(ota)
+    cringe_jsons = [
+        '1735027826.json',
+        '1737963944.json',
+        '1738137293.json',
+        '1739607521.json',
+    ]
+
+    if codename == 'daisy' and any(
+        ota_json_file.name.endswith(x) for x in cringe_jsons
+    ):
+        ota_json_file.write_text(ota_daisy)
+    else:
+        ota_json_file.write_text(ota)
 
 # Write a dummy ota
 dummy_ota = """{
