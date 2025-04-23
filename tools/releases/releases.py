6aab6e25e8123b4111e96a36e9336d5431fc1fee
Giovanni Ricca <giovanniricca@proton.me>
2025-04-23 16:32:34 +0200

    God forbids a nairo in calabria ...
    Change-Id: I64aad0c97758b35f8e4635c28b9822a5ff70c606

--- tools/releases/releases.py
+++ tools/releases/releases.py
@@ -70,6 +70,7 @@ def get_device(var):
         'gemstone': {1: 'Redmi Note 12 5G', 2: '22.2', 3: 'LineageOS_gemstone'},
         'lisa': {1: 'Xiaomi 11 Lite 5G NE', 2: '22.2', 3: 'LineageOS_lisa'},
         'miatoll': {1: 'Xiaomi Atoll Family', 2: '22.2', 3: 'LineageOS_miatoll'},
+        'nairo': {1: 'Moto G 5G Plus', 2: '22.2', 3: 'LineageOS_nairo'},
         'sakura': {1: 'Redmi 6 Pro', 2: '22.2', 3: 'LineageOS_sakura'},
         'xaga': {1: 'POCO X4 GT', 2: '22.2', 3: 'LineageOS_xaga'},
         'ysl': {1: 'Redmi S2/Y2', 2: '22.2', 3: 'LineageOS_ysl'},
