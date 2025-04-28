cc37753dc9c413797ab1b81415c5c8872525ee7a
Giovanni Ricca <giovanniricca@proton.me>
2025-04-28 23:11:59 +0200

    Drop gemstone
    Change-Id: I0c78403e136d7ff2cf37fb5a011e46f808023c77

--- tools/releases/releases.py
+++ tools/releases/releases.py
@@ -67,7 +67,6 @@ def get_device(var):
     return {
         # LineageOS 22.2
         'daisy': {1: 'Mi A2 Lite', 2: '22.2', 3: 'LineageOS_daisy'},
-        'gemstone': {1: 'Redmi Note 12 5G', 2: '22.2', 3: 'LineageOS_gemstone'},
         'lisa': {1: 'Xiaomi 11 Lite 5G NE', 2: '22.2', 3: 'LineageOS_lisa'},
         'miatoll': {1: 'Xiaomi Atoll Family', 2: '22.2', 3: 'LineageOS_miatoll'},
         'nairo': {1: 'Moto G 5G Plus', 2: '22.2', 3: 'LineageOS_nairo'},
