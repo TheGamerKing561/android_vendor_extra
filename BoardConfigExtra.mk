24bf46d1ece3a5a1cde3f7af22829a783810b7ff
Giovanni Ricca <giovanniricca@proton.me>
2025-05-16 09:14:11 +0200

    build: patches: Add `sun` support
    Change-Id: I228dcce5aabd70e248c06a12ecb7bc173ed1d3bb

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -6,5 +6,13 @@
 # Inherit MiuiCamera Makefile
 -include vendor/xiaomi/miuicamera-$(PRODUCT_DEVICE)/BoardConfig.mk
 
-# SELinux
+# Inherit SELinux Makefile
 include $(VENDOR_EXTRA_PATH)/sepolicy/SEPolicy.mk
+
+# Soong namespace
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifneq ($(TARGET_BOARD_PLATFORM),sun)
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/thermal
+endif #TARGET_BOARD_PLATFORM
+endif #BOARD_USES_QCOM_HARDWARE
