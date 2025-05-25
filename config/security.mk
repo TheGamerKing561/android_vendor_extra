0f6b1e64e62be5f05db431e3cea615d1841e02b5
Giovanni Ricca <giovanniricca@proton.me>
2025-05-25 16:27:51 +0200

    config: security: Spoof build flavor prop
    Change-Id: I217a9800740017ab23e0756725c6f3f31d13d4bb

--- config/security.mk
+++ config/security.mk
@@ -11,5 +11,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFlavor="$(VENDOR_EXTRA_TARGET_DEVICE)-$(TARGET_BUILD_VARIANT)" \
     LineageDesc="$(BUILD_ID)"
 endif
