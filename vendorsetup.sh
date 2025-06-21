d74a3c2cddd02ce77bbd09b80214a4e6e8dfd758
Giovanni Ricca <giovanniricca@proton.me>
2025-06-21 10:36:48 +0200

    vendorsetup.sh: mka_kernel: Add venus
    Change-Id: I3131fab077b937b324d155187995300f5b9c6dac

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -172,6 +172,7 @@ function mka_kernel() {
     device_kernel_targets["gemstone"]="bootimage dtboimage vendorbootimage"
     device_kernel_targets["lisa"]="bootimage dtboimage vendorbootimage vendor_dlkmimage"
     device_kernel_targets["miatoll"]="bootimage dtboimage"
+    device_kernel_targets["venus"]="bootimage dtboimage vendorbootimage vendor_dlkmimage"
     device_kernel_targets["xaga"]="bootimage vendorbootimage vendor_dlkmimage"
 
     kernel_targets=${device_kernel_targets[$DEVICE]:-"bootimage"}
