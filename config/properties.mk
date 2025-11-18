3293e97a21eb342c6db8d9219c9a9ce726957252
Giovanni Ricca <giovanniricca@proton.me>
2025-11-18 17:41:55 +0100

    build: patches: lineage-23: Update patches
    Change-Id: Ic642e852c528c5ddf2ed07efd81323d4f127a413

--- config/properties.mk
+++ config/properties.mk
@@ -11,9 +11,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # OEM Unlock reporting
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.oem_unlock_supported=0
-
-ifneq ($(filter xaga,$(VENDOR_EXTRA_TARGET_DEVICE)),)
-# Display
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.activity_anim_perf_override=true
-endif
